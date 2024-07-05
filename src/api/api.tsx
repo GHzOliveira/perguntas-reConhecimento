3c0a9c20c912bc038aa5b69002b97d2ff5a706a6
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2024-07-04 22:24:39 -0300

    testando api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://perguntas-reconhecimento-api-production.up.railway.app",
+  baseURL: "",
 });
 
 export const createFilial = async (filial: string, quantidadeColaboradores: number) => {
