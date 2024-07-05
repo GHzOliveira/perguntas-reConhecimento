132a5764bc71dbd51bf71dc0d462b4ecc8f1c785
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2024-07-04 22:18:49 -0300

    mudando api para produção

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "perguntas-reconhecimento-api-production.up.railway.app",
+  baseURL: "http://perguntas-reconhecimento-api-production.up.railway.app",
 });
 
 export const createFilial = async (filial: string, quantidadeColaboradores: number) => {
