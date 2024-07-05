ef0c6e87ff4c6323809add942fa1800ad50b4da8
Guilherme Oliveira <gh.oliveira1325@gmail.com>
2024-07-05 13:10:07 -0300

    ajustar textos e cores

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Image } from '@chakra-ui/react';
 import logo from '../../assets/logo/logo_capital.svg';
 
 export const Footer = () => (
-  <Box as="footer" width="full" padding="2" marginTop="8" backgroundColor={'#0AAE4B'}>
+  <Box as="footer" width="full" padding="2" marginTop="8" backgroundColor={'#1F7CBF'}>
     <Image src={logo} boxSize={"100px"} marginLeft={"5rem"}/>
   </Box>
 );
