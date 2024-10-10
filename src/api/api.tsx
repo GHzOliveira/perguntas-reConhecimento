8e5f96823b3c965c2dc93d273d437344d5cd18ff
unknown <gh.oliveira1325@gmail.comgit config --global user.email gh.oliveira1325@gmail.com>
2024-10-10 01:35:17 -0300

    feat: nova versão tabela

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'perguntas-reconhecimento-api-production.up.railway.app'
   // responseType: 'blob'
 })
 
